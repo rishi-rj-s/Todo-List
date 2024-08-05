b8e0d22cbf527629575b5d83d33a225f684b41c5
rishi-rj-s <rishisanu007@gmail.com>
2024-08-05 12:37:45 +0530

    Update 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,30 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-
+import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectUserId = 'u1';  
 
   onSelect(id:string){
     alert('Selected User with ID: '+id);
+  }
+
+  get selectedUser(){
+    return this.users.find((user)=>user.id === this.selectUserId)!;
+  }
+  
+  onSelectedUser(id:string){
+    this.selectUserId = id;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,12 @@
 import { Component, computed, EventEmitter, input, Input, Output, output} from '@angular/core';
 
 
+interface User{
+  id : string,
+  avatar: string,
+  name: string
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,21 +14,21 @@ import { Component, computed, EventEmitter, input, Input, Output, output} from '
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
 
-  @Input({required:true}) id !: string;
- @Input({required:true}) avatar !: string;
- @Input({required:true}) name !: string;
-//  @Output() select = new EventEmitter<string>();
-select = output<string>();
+  @Input({required:true}) user !: User;
+  
+ @Output() select = new EventEmitter<string>();
+// select = output<string>();
 
 
   get imagePath(){
-    return 'assets/users/'+this.avatar;
+    return 'assets/users/'+this.user.avatar;
   }
 
   alertIt():void{
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
 
